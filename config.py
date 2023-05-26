379e7e2344209a831573fb18c46c39ef16f4b9d2
SFAS0 <sfas7@mail.ru>
2023-05-26 19:18:33 +0300

    bot

--- config.py
+++ config.py
@@ -1,6 +1,24 @@
 TOKEN = ''  # токен бота
 token_vk = ''  # токен вк для парсинга
 chat_id = ''  # chat id группы или лс
-name_group = {'englishhouse35': 'English House - Английский язык',
-              'englishhouse_vologda': 'English House Вологда - Английский язык',
-              'englishhouse_che': 'English House Череповец - Английский язык'}  # список групп
+# список групп c токенами
+name_group = {'englishhouse35': ('English House - Английский язык', 'vk1.a.9WcAFsns-8NsO34YsP8EjfgQSfayQTaboM2VdssqGN6'
+                                                                    '_CCNa6_8PUw9cuyRzJRrq9qID78Re1ELOPhbmQGyKSEO726sL'
+                                                                    'da1RlgaxSxSTKfa84tkCsfw_0CsHA2f3bNsV1vXdr0nCdYfma'
+                                                                    'tatW0ZBGZ-JIzYXNu5USRQQ7oNSJwPJ-7Yw13tUFthirno6P-'
+                                                                    'U3buMcM-I4NgkMyOw7zJwL9Q'),
+              'englishhouse_vologda': ('English House Вологда - Английский язык', 'vk1.a.E2BA5i_VQhe1fCGtA7rVpCej5TcPu'
+                                                                                  '89DOojEK5XEPdEYVRCvK4XImrn3PmcM3i0V'
+                                                                                  'qmNsWpPID26oq1gYGxqtNreBk8BAeQdG9fxT'
+                                                                                  'FR_VKRXxAnXhglTgiXZA7D0ONYX-n3yO6Cs'
+                                                                                  'S1H4Nbr2qSxXIO6kr-ApbGvx29Q6qqCR1DRw'
+                                                                                  'XbTo_rTZ9HVk_InyanxhYCBvqXAqlSRKoz5Q'
+                                                                                  'p1R9lnA'),
+              'englishhouse_che': ('English House Череповец - Английский язык', 'vk1.a.WrHKIpSr3hwt3gjtWwGfyz1RvZW3n'
+                                                                                  'VqhQClQw0ap2Q2mOB88A26atKgrVX_nnPC3q'
+                                                                                  'npS7Y6vLPQB0nT34jG62C2pJDscxMk1E_UHL'
+                                                                                  'fQVIpw05Lwf7BNKnMUKGeqabO1UBDn1Xfzx'
+                                                                                  'VfV6HcjiZNnmJQhsITrkLG7gS43-rDH6orSZ'
+                                                                                  'usB9hMRt8988pujRHepDJHtL1BPCyQUtge1u'
+                                                                                  'z2XuZA')}
+
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
+from vk_api.longpoll import VkLongPoll, VkEventType
 from flask_apscheduler import APScheduler
 from flask import Flask
 from datetime import datetime, timedelta
-from data import db_session
-from config import token_vk, TOKEN, chat_id, name_group
-from data.posts import Posts
+from config_my import token_vk, TOKEN, chat_id, name_group
 import requests
+import vk_api
+import threading
 
 
 app = Flask(__name__)
+
+
+def check_messege_group(token_vk_group, name_group_mess):
+    vk = vk_api.VkApi(token=token_vk_group)
+    longpoll = VkLongPoll(vk)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            if event.to_me:
+                # Сообщение от пользователя
+                request = event.text
+                send_message(chat_id, f'В группу {name_group_mess} пришло сообщение: {request}')
+                print(request)
 
 
 class Config:
@@ -15,43 +28,6 @@ class Config:
 
 
 scheduler = APScheduler()
-
-
-@scheduler.task("interval", id="check_new_post", seconds=1800, misfire_grace_time=1800)
-def last_post_in_db():
-    posts = [[post.id, post.post_id, post.views_count, post.id_group] for post in db.query(Posts).all()]
-    for group in name_group.keys():
-        response = requests.get('https://api.vk.com/method/wall.get', params={'access_token': token_vk,
-                                                                          'v': 5.91, 'domain': group,
-                                                                          'count': 1})
-        data = response.json()
-        post_id = data['response']['items'][0]['id']
-        views_count = data['response']['items'][0]['views']['count']
-        id_group = group
-        fl = True
-        fl_old_post = False
-        for post in posts:
-            if post_id in post:
-                fl = False
-            if id_group in post:
-                fl_old_post = True
-        if fl:
-            if fl_old_post:
-                # Удаление старого последнего поста
-                print('delete')
-                db.query(Posts).filter(Posts.id_group == id_group).delete(synchronize_session='fetch')
-            # добавляем новый пост в бд (последний из группы)
-            posts_class = Posts()
-            posts_class.post_id = post_id
-            posts_class.views_count = views_count
-            posts_class.id_group = id_group
-            db.add(posts_class)
-            db.commit()
-            print('add post')
-            text = data['response']['items'][0]['text'][:30]
-            send_message(chat_id, f'{name_group[id_group]}: \n {text}')
-        else:
-            pass
 
 
 def send_message(chat_id, text):
@@ -68,22 +44,25 @@ def receive_update():
 
 @scheduler.task("interval", id="daily_report", seconds=30, misfire_grace_time=30)
 def send_daily_report():
+    proxies = {
+        'https': 'socks5://198.50.217.202:1080'
+    }
     if int(datetime.now().time().hour) == 10 and int(datetime.now().time().minute) == 00:
         last_weeks_posts = {}
         date_week_later = (datetime.today() - timedelta(days=7)).strftime('%Y %m %d')
         for group in name_group.keys():
             response = requests.get('https://api.vk.com/method/wall.get', params={'access_token': token_vk,
                                                                                   'v': 5.91, 'domain': group,
-                                                                                  'count': 100})
+                                                                                  'count': 100}, proxies=proxies)
             data = response.json()
             for post in data['response']['items']:
                 date = datetime.utcfromtimestamp(int(post['date'])).strftime('%Y %m %d')
                 if date > date_week_later:
                     views_count = post['views']['count']
-                    if name_group[group] in last_weeks_posts.keys():
-                        last_weeks_posts[name_group[group]].append(views_count)
+                    if name_group[group][0] in last_weeks_posts.keys():
+                        last_weeks_posts[name_group[group][0]].append(views_count)
                     else:
-                        last_weeks_posts[name_group[group]] = [views_count]
+                        last_weeks_posts[name_group[group][0]] = [views_count]
         answer = ''
         sum_all_posts_views = []
         for group_name, list_views_count in last_weeks_posts.items():
@@ -98,9 +77,9 @@ def send_daily_report():
 
 
 if __name__ == '__main__':
-    db_session.global_init('db/bot_db.sqlite')
-    db = db_session.create_session()
-    last_post_in_db()
+    for id, (name, token) in name_group.items():
+        group_check = threading.Thread(target=check_messege_group, args=(token, name))
+        group_check.start()
     app.config.from_object(Config())
     scheduler.init_app(app)
     scheduler.start()
